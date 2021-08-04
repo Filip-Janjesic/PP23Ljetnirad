8b737a95d74ee933341a4d4743aad1e34bb49b05
Filip Janješić <83244348+Filip-Janjesic@users.noreply.github.com>
2021-08-04 17:10:00 +0200

    ljetni rad

--- index.php
+++ index.php
@@ -1,19 +1,60 @@
 <!DOCTYPE html>
-<html>
-    <head>
-        <title>Login</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-    </head>
-    <body>
-        <center>
-            <div class="floating-box">
-                <form name="form1" method="post" action="login/run">
-                <label for="uname">User Name</label>
-                    <input type="text" id="user_name" name="user_name"><br><br>
-                <label for="pwd">Password</label>
-                    <input type="password" id="password" name="password"><br><br>
-                    <input name="submit" type="submit" id="submit" value="Login"><br>
-    <p>New User <a href="signup.php">Register Here</a></p>
-        </center>
-    </body>
+<html lang="hr">
+<head>
+<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png">
+     <link rel="stylesheet" href="./assets/css/foundation.css" />
+     <link rel="stylesheet" href="./assets/css/style.css">
+    <title> OPG planer </title>
+    <link rel="shortcut icon" href="./favicon.ico" type="image/x-icon">
+    <link rel="icon" href="./favicon.ico" type="image/x-icon">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Kristi&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Hahmlet:wght@100&family=Kristi&display=swap" rel="stylesheet">
+</head>
+<body class="index">
+<div class="top-bar">
+  <div class="top-bar-left">
+    <ul class="dropdown menu" data-dropdown-menu>
+      <li class="menu-text">OPG planer</li>
+      <a href="../index.php">Početna</a>
+      <li class="has-submenu">
+        <a href="#0">Usluge</a>
+        <ul class="submenu menu vertical" data-submenu>
+          <li><a href="#0">Punjenje klima</a></li>
+          <li><a href="#0">Mehanika</a></li>
+          <li><a href="#0">Autoelektrika/Diagnostika</a></li>
+        </ul>
+      </li>
+      <li><a href="../klijent/klijentP.php">Korisnik</a></li>
+      <li><a href="admin.php">Admin</a></li>
+    </ul>
+  </div>
+</div>
+<div class="container">
+        <div class="row">
+          <div class="col-md-3 col-sm-6 text-center">
+            <div class="wrap animated" data-animate="fadeInDown">
+              <div class="img-team">
+                <img src="./img/car3.jpg" alt="" class="img-circle">
+              </div>
+              <h1 class="OPGplanerNaslov"> OPG planer </h1>
+          </div>
+        </div>
+      </div> <!-- /.container -->
+      
+<!--fondation js-->
+<script src="assets\js\vendor.js"></script>
+    <script src="assets\js\foundation.js.map"></script>
+    <script src="assets\js\foundation.js"></script>
+    <script>
+      $(document).foundation();
+    </script>
+<p class="gitlink"><a href="https://github.com/Filip-Janjesic/PP23Ljetnirad"> Filip Janješić </a></p><a href="https://github.com/Filip-Janjesic/PP23Ljetnirad"></a>
+</body>
 </html>
